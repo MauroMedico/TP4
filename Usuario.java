234a5c848cf59392c62e86e302b33a789db91901
Mauro Medico <mauromedico@Hotmail.com>
2020-05-28 00:57:55 -0300

    Se agrego un codigo basico en java

---
+++ Usuario.java
@@ -0,0 +1,8 @@
+public class Usuario {
+  public static void main(String[] args){
+    System.out.println("Tabla 12");
+    for(int i = 0; i <= 12; i++){
+    System.out.println("12 * "+ i + " = " + 12 * i + "\n");
+   }
+  }
+}
