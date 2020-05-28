1ec002e521725062a30da209d32c742884a95423
MauroMedico <65430984+MauroMedico@users.noreply.github.com>
2020-05-28 08:57:02 -0300

    Delete Usuario.java

--- Usuario.java
+++
@@ -1,8 +0,0 @@
-public class Usuario {
-  public static void main(String[] args){
-    System.out.println("Tabla 12");
-    for(int i = 0; i <= 12; i++){
-    System.out.println("12 * "+ i + " = " + 12 * i + "\n");
-   }
-  }
-}
